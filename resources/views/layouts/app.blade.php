0491221fdfba5a4cadfe8254a7a75210e37de934
jon3laze <jonaouellette@gmail.com>
2016-04-15 12:37:53 -0700

    Update layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>@yield('title')</title>
 
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
@@ -13,49 +13,38 @@
 
     <!-- Styles -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
 
-    <style>
-        body {
-            font-family: 'Lato';
-        }
-
-        .fa-btn {
-            margin-right: 6px;
-        }
-    </style>
 </head>
 <body id="app-layout">
-    <nav class="navbar navbar-default navbar-static-top">
+    <!--Navbar-->
+    <nav class="navbar navbar-default">
         <div class="container">
             <div class="navbar-header">
-
-                <!-- Collapsed Hamburger -->
+                <!--Collapsed Hamburger-->
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                     <span class="sr-only">Toggle Navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
+                <!--/Collapsed Hamburger-->
 
-                <!-- Branding Image -->
+                <!--Branding Image-->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
+                    <strong>Network</strong>.run
                 </a>
+                <!--/Branding Image-->
             </div>
 
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
-                </ul>
-
-                <!-- Right Side Of Navbar -->
+                <!--Right Side Of Navbar-->
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
+                        @if(url('/login') !== Request::url())
+                            <li><a href="{{ url('/login') }}">Login</a></li>
+                        @endif
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -63,20 +52,57 @@
                             </a>
 
                             <ul class="dropdown-menu" role="menu">
+                                @if(route('home') !== Request::url())
+                                    <li><a href="{{ url('/') }}"><i class="fa fa-btn fa-home"></i>Dashboard</a></li>
+                                @endif
+                                @if(route('settings') !== Request::url())
+                                    <li><a href="{{ url('/settings') }}"><i class="fa fa-btn fa-gear"></i>Settings</a></li>
+                                @endif
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                             </ul>
                         </li>
                     @endif
                 </ul>
+                <!--/Right Side Of Navbar-->
             </div>
         </div>
     </nav>
-
-    @yield('content')
-
+    <!--/Navbar-->
+    <!--Flash Message-->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel-body">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/Flash Message-->
+    <!--Container-->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+    <!--/Container-->
+    <!--Footer-->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel-body">
+                    <div class="well well-sm text-center">
+                        <small><i class="fa fa-copyright fa-fw text-muted"></i>Network.run</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/Footer-->
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+    <script src="{{ elixir('js/app.js') }}"></script>
 </body>
 </html>
